67c7576deb28efa51b07f6f41d9df17842dd6052
aaronsato <2asato@gmail.com>
2020-09-30 14:12:43 -0700

    challenge - created question constructor

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -144,19 +144,226 @@
 
 
 // FUNCTIONS
-// -   also object
+// -   also an object
 // -   an instance of teh Object type
 // -   behaves like any other Object
 // -   can store functions in a variable
 // -   can pass a function as an argument to another function
 // -   can return a function from a function
 
+// passing functions as arguments
+// var years = [1990, 1965, 1937, 2005, 1998];
+
+// function arrayCalc(arr, fn) {
+//     var arrRes = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// }
+
+// function calculateAge(el) {
+//     return 2020 - el;
+// }
+
+// function isFullAge(el) {
+//     return el >= 18;
+// }
+
+// function maxHeartRate(el) {
+//     if (el >= 18 && el <= 81) {
+//         return Math.round(206.9 - (0.67 * el));
+//     }
+//     return -1;
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// console.log(ages);
+// var fullAges = arrayCalc(ages, isFullAge)
+// console.log(fullAges);
+// var rates = arrayCalc(ages, maxHeartRate);
+// console.log(rates);
+
+// functions returning functions
+// function interviewQs(job) {
+//     if (job === 'designer') {
+//         return function(name) {
+//             console.log(name + ', can you please explain what UX design is?');
+            
+//         } 
+
+//     } else if(job === 'teacher') {
+//         return function(name) {
+//             console.log('What subject do you teach, ' + name + '?');
+            
+//         }
+//     } else {
+//         return function(name) {
+//             console.log('Hello ' + name + ', what do you do?');
+            
+//         }
+//     }
+// }
+
+// var teacherQuestion = interviewQs('teacher');
+// teacherQuestion('John');
+// var designerQuestion = interviewQs('designer');
+// designerQuestion('John');
+// interviewQs('student')('Mark')
+
+
+// IIFE
+// immediately invoked function expression
+
+// function game() {
+//     var score = Math.random() * 10;
+//     console.log(score >= 5);
+    
+// }
+// game();
+
+// // better way
+// (function() {
+//         var score = Math.random() * 10;
+//         console.log(score >= 5);    
+//     }
+// )();
+
+
+// CLOSURES
+
+// function retirement(retirementAge) {
+//     var a = ' years left until retirement.';
+//     return function(yearOfBirth) {
+//         var age = 2020 - yearOfBirth;
+//         console.log((retirementAge - age) + a);
+        
+//     }
+// }
+
+// var retirementUS = retirement(66);
+// var retirementGermany = retirement(65);
+// var retirementIceland = retirement(67);
+
+// retirementUS(1990);
+// retirementGermany(1990);
+// retirementIceland(1990);
+
+// -   an inner function has always access to the variables and parameters 
+//     of its outer function, even after the outer function has returned
+
+// function interviewQs(job) {
+//     if (job === 'designer') {
+//         return function(name) {
+//             console.log(name + ', can you please explain what UX design is?');
+            
+//         } 
+
+//     } else if(job === 'teacher') {
+//         return function(name) {
+//             console.log('What subject do you teach, ' + name + '?');
+            
+//         }
+//     } else {
+//         return function(name) {
+//             console.log('Hello ' + name + ', what do you do?');
+            
+//         }
+//     }
+// }
+
+// function interviewQs(job) {
+//     return function(name) {
+//     if (job === 'designer') {
+//             console.log(name + ', can you please explain what UX design is?');
+            
+
+
+//     } else if(job === 'teacher') {
+//             console.log('What subject do you teach, ' + name + '?');
+            
+        
+//     } else {
+//             console.log('Hello ' + name + ', what do you do?');
+            
+//         }
+//     }
+// }
+
+// interviewQs('janitor')('Stan');
+
+
+// BIND, CALL, AND APPLY
+
+// var john = {
+//     name: 'John',
+//     age: 26,
+//     job: 'teacher',
+//     presentation: function(style, timeOfDay) {
+//         if (style === 'formal') {
+//             console.log('Good ' + timeOfDay + ' Ladies and Gentleman! I\'m ' + this.name + ' and I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+            
+//         } else if (style === 'friendly') {
+//             console.log('Hey whats up! I\'m ' + this.name + ' and I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+            
+//         }
+//     }
+    
+// }
+
+// var emily = {
+//    name: 'Emily',
+//    age: 35,
+//    job: 'designer' 
+// };
+
+// john.presentation('formal', 'morning');
+
+// john.presentation.call(emily, 'friendly', 'afternoon')
+
+// // john.presentation.apply(emily, ['friendly', 'afternoon'])
+
+// use funciton with preset arguemnt
+// var johnFriendly = john.presentation.bind(john, 'friendly');
+// johnFriendly('morning');
+
+// var emilyFormal = john.presentation.bind(emily, 'formal');
+// emilyFormal('night');
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
 
 
 
+// var Receiver = function(name, catches, yards) {
+//     this.name = name;
+//     this.catches = catches;
+//     this.yards = yards;
+// }
 
-
-
-
+var Question = function(questionAsked, answerChoices, correctAnswer) {
+    this.questionAsked = questionAsked;
+    this.answerChoices = [];
+    this.correctAnswer = correctAnswer;
+}
 
 
