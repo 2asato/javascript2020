25057bc388a2b3c2fcc2505f1d8ce8b43477a8f4
aaronsato <2asato@gmail.com>
2020-10-08 11:20:01 -0700

    quit game prompt showing after question answered

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -404,18 +404,21 @@ function showQuestion() {
     // answer choices to random question
     console.log('1. ' + randomQuestion.answerChoices[0].choice);
     console.log('2. ' + randomQuestion.answerChoices[1].choice); 
+    
+    
     // 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
     answerPrompt = prompt(randomQuestion.questionAsked, 'Enter the number of the correct answer')
     // console.log(answerPrompt);
     checkAnswer();
 }
 
-
+// quitGame()
 showQuestion()
+
 
 // 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 function checkAnswer() {
-    console.log(randomQuestion.correctAnswer);
+    // console.log(randomQuestion.correctAnswer);
     console.log('answer prompt = ' + answerPrompt);
     
     if (answerPrompt == randomQuestion.correctAnswer) {
@@ -425,14 +428,14 @@ function checkAnswer() {
         console.log('WRONG!!!');
         
     }
-    showQuestion();
+    // 8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+    quitGame();
 }
 
 // 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 
 // --- Expert level ---
 
-// 8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
 
 // 9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
 
@@ -441,3 +444,8 @@ function checkAnswer() {
 // 11. Display the score in the console. Use yet another method for this.
 
 
+function quitGame() {
+    console.log('working!');
+    
+    prompt('Do you want to keep playing?', 'y for yes, n for no');
+}
